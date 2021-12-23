389e738626deea161f2fc9b54bfbb0c9ff46d3cf
Cyph <96255759+Cyphrous@users.noreply.github.com>
2021-12-23 21:48:39 +0800

    Create MainW.lua

---
+++ Main/MainW.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 5630129588 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Cyphrous/Scripts/main/Main/Sources/Smash%20Legends.lua"))()
+else
+game.Players.LocalPlayer:Kick ("Game not Supported\nby MoonHub")
+end
