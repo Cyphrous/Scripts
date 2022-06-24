82bfa26eca3a42034d27608d9d8b5357566abd45
Cyph <96255759+Cyphrous@users.noreply.github.com>
2022-06-24 17:04:07 +0800

    Update 4BTCMnr.lua

--- Main/4BTCMnr.lua
+++ Main/4BTCMnr.lua
@@ -1,4 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Cyphrous/Scripts/main/Main/Sources/BitcoinMiner.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/juNstring/LuaProjects/main/AutoFarmBitcoinMiner.lua"))()
 
